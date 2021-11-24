18f60281be6d9c499e1d2e474487d93c94026b63
@Elias <eliasonofre2013@gmail.com>
2021-11-23 21:24:15 -0500

    feat: ensure should calls cpf value correct

--- src/presentation/controllers/register/register.ts
+++ src/presentation/controllers/register/register.ts
@@ -1,15 +1,26 @@
-import { HttpRequest, HttpResponse } from '../../protocols/http'
-import { Controller } from '../../protocols/controller'
-import { MissingParamError } from '../../errors/missing-param-error'
+import { HttpRequest, HttpResponse, Controller } from '../../protocols'
+import { CpfValidator } from './register-protocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 
 export class RegisterController implements Controller {
+  private readonly cpfValidator: CpfValidator
+
+  constructor (cpfValidator: CpfValidator) {
+    this.cpfValidator = cpfValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const fieldRequest = ['cpf', 'phone', 'cep']
     for (const field of fieldRequest) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const { cpf } = httpRequest.body
+    const isValid = this.cpfValidator.isValid(cpf)
+    if (!isValid) {
+      return badRequest(new InvalidParamError(cpf))
     }
   }
 }
