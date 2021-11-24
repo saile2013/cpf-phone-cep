368231c2c9fc73c6353f2ea3f2b0bbe47af647b2
@Elias <eliasonofre2013@gmail.com>
2021-11-23 21:24:42 -0500

    test: ensure should calls cpf value correct

--- src/presentation/controllers/register/register.spec.ts
+++ src/presentation/controllers/register/register.spec.ts
@@ -1,9 +1,33 @@
 import { RegisterController } from './register'
-import { MissingParamError } from '../../errors/missing-param-error'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { CpfValidator } from './register-protocols'
+
+const makeCpfValidator = (): CpfValidator => {
+  class CpfValidorStub implements CpfValidator {
+    isValid (cpf: string): boolean {
+      return true
+    }
+  }
+  return new CpfValidorStub()
+}
+
+interface SutTypes {
+  sut: RegisterController
+  cpfValidatorStub: CpfValidator
+}
+
+const makeSut = (): SutTypes => {
+  const cpfValidator = makeCpfValidator()
+  const sut = new RegisterController(cpfValidator)
+  return {
+    sut: sut,
+    cpfValidatorStub: cpfValidator
+  }
+}
 
 describe('Register Controller', () => {
   test('Should return 400 if no cpf is provided', () => {
-    const sut = new RegisterController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         phone: 'any_phone',
@@ -16,7 +40,7 @@ describe('Register Controller', () => {
   })
 
   test('Should return 400 if no phone is provided', () => {
-    const sut = new RegisterController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         cpf: 'any_cpf',
@@ -29,7 +53,7 @@ describe('Register Controller', () => {
   })
 
   test('Should return 400 if no cep is provided', () => {
-    const sut = new RegisterController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         cpf: 'any_cpf',
@@ -39,5 +63,34 @@ describe('Register Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('cep'))
+  })
+
+  test('Should return 400 if an invalid cpf is provided', () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    jest.spyOn(cpfValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        cpf: 'invalid_cpf',
+        phone: 'any_phone',
+        cep: 'any_cep'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError(httpRequest.body.cpf))
+  })
+
+  test('Should calls CPF value correct', () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    const cpfSpy = jest.spyOn(cpfValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        cpf: 'invalid_cpf',
+        phone: 'any_phone',
+        cep: 'any_cep'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(cpfSpy).toHaveBeenCalledWith(httpRequest.body.cpf)
   })
 })
